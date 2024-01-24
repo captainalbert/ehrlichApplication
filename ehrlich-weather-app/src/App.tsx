9ebc66bee30a6e04ae4caa14629c3f508909ae19
Albert John Tulop <albertjohntulop@VeritasPays-MacBook-Pro-2.local>
2024-01-25 00:42:12 +0800

    Initial commit: Project setup & Add NavBar component

---
+++ ehrlich-weather-app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div>
+      <Navbar isAuthenticated />
+    </div>
+  );
+}
+
+export default App;
---
+++ ehrlich-weather-app/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,49 @@
+/**
+ * This component is used for navigation bar of the website.
+ *
+ * @argument isAuthenticated - boolean
+ * @author Albert John Tulop
+ */
+import React, { useState } from "react";
+import "./NavBar.css";
+
+interface NavbarProps {
+  // You can define any additional props here
+  isAuthenticated: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isAuthenticated = true }) => {
+  // State for showing navigationBar
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
+
+  // Function to toggle the navbar state
+  const toggleNavbar = () => {
+    setNavbarOpen(!isNavbarOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-brand">
+          Weather App
+        </a>
+
+        {/* Hamburger menu for mobile view */}
+        <div className="navbar-toggle" onClick={toggleNavbar}>
+          ☰
+        </div>
+
+        {/* Logout element */}
+        {isAuthenticated && (
+          <div className={`navbar-links ${isNavbarOpen ? "open" : ""}`}>
+            <a href="/" className="navbar-link" onClick={toggleNavbar}>
+              logout
+            </a>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
