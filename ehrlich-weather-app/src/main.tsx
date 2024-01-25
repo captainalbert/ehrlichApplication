e85a4d345eabcfa055925a463967a6f55c300196
Albert John Tulop <albertjohntulop@VeritasPays-MacBook-Pro-2.local>
2024-01-25 09:44:39 +0800

    Integrate Auth0 provider

--- ehrlich-weather-app/src/main.tsx
+++ ehrlich-weather-app/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-0pttj0dkc07rpfa4.us.auth0.com"
+      clientId="H6eDaJZSkFfO4WmoUJaoWG0Gr72iVLQ8"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
--- ehrlich-weather-app/src/views/LandingPage/LandingPage.tsx
+++ ehrlich-weather-app/src/views/LandingPage/LandingPage.tsx
@@ -3,8 +3,10 @@ import "./LandingPage.css";
 
 import Button from "../../components/Button/Button";
 import Title from "../../components/Title/Title";
+import LoginButton from "../../components/LoginButton/LoginButton";
 
 const LandingPage = () => {
+  // This function is responsible for handling login.
   const handleLogin = () => {
     console.log("user tried to login");
   };
@@ -15,7 +17,8 @@ const LandingPage = () => {
         title="Welcome to the weather forecast web application. Please login with your
         GitHub user to use the application and view the weather in your city"
       />
-      <Button title="Login" onClick={handleLogin} />
+      {/* <Button title="Login" onClick={handleLogin} /> */}
+      <LoginButton />
     </div>
   );
 };
