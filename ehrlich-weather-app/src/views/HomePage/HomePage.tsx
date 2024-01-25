731853045132cff8b151f6a3f42447ea92684062
Albert John Tulop <albertjohntulop@VeritasPays-MacBook-Pro-2.local>
2024-01-25 11:48:11 +0800

    Create button for displaying weather

--- ehrlich-weather-app/src/views/HomePage/HomePage.tsx
+++ ehrlich-weather-app/src/views/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import "./HomePage.css";
 import Title from "../../components/Title/Title";
 import TextInput from "../../components/TextInput/TextInput";
 import { useState } from "react";
+import Button from "../../components/Button/Button";
 
 type userObject = {
   name: string;
@@ -28,6 +29,12 @@ const HomePage = ({ user }: HomePageProps) => {
         placeholder="City"
         onChange={(e) => setCity(e.target.value)}
         value={city}
+      />
+
+      <Button
+        title="Display Weather"
+        onClick={() => console.log("Display weather")}
+        style={{ alignSelf: "center", marginTop: 20 }}
       />
     </div>
   );
